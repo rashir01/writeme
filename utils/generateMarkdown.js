3c95596e3f611dd8fe8b3c85b4b2e1a77b251d38
Ray Ashir <rayeed.ashir@gmail.com>
2021-08-16 07:20:37 -0700

    added comments to generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,22 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+/*
+  Function: renderLicenseBadge
+  Usage: Used to get the url of the license's image. If unsupported license, an empty string is returned
+  input: license - string representing license. It can be either "Apache", "GNU", or "MIT". 
+  return: string - url of the license if it is valid or an empty string otherwise
+*/
 function renderLicenseBadge(license) {
   if(license.toLowerCase() === "apache") return 'https://img.shields.io/badge/License-Apache%202.0-blue.svg';
   if(license.toLowerCase() === "gnu") return 'https://img.shields.io/badge/License-GPLv3-blue.svg';
-  if(license.toLowerCase() === "mit") return 'https://img.shields.io/badge/License-MIT-yellow.svg'
+  if(license.toLowerCase() === "mit") return 'https://img.shields.io/badge/License-MIT-yellow.svg';
   return "";
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+/*
+  function: renderLicenseLink
+  Usage: used to get the url of the license. If unsupported license, an empty string is returned
+  input: license - string representing the license. Supported licenses are Apache, GNU, or MIT
+  return: string - url of the license or empty string if invalid
+*/
 function renderLicenseLink(license) {
   if(license.toLowerCase() === "apache") return 'https://opensource.org/licenses/Apache-2.0';
   if(license.toLowerCase() === "gnu") return 'https://www.gnu.org/licenses/gpl-3.0';
@@ -16,19 +24,18 @@ function renderLicenseLink(license) {
   return "";
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+/* 
+  function: renderLicenseSection
+  purpose: creates the license section's string of the generated README file
+  input: license - string representing the license. Supported are apache, gnu or mit
+  return: string - the markdown string for the license if supported, or empty string otherwise
+*/
 function renderLicenseSection(license) {
-
-  license = license[0] ;
   if (license === "Apache" || license === "GNU" || license === "MIT") {
     console.log("inside if statement");
     return `[![License](${renderLicenseBadge(license)})](${renderLicenseLink(license)})`;
   } 
-  
   return "";
-  
-
 }
 
 /*
@@ -41,38 +48,38 @@ function generateMarkdown(data) {
   //add page title
   let titleString =  `# ${data.Title}\n`;
   //add TOC
-  let toc = "## Table of Contents\n";
+  let tocString = "## Table of Contents\n";
   
   //add license if available
   let licenseString = "";
 
   if (data['License'] != "") {
     licenseString = "## License\n";
-    toc = toc + "[License](#license)\n\n";
+    tocString = tocString + "[License](#license)\n\n";
     licenseString = licenseString + `This project is covered by the ${data.License} license\n\n`;
 
-    let licenseMarkup = renderLicenseSection(data['License']);
+    let licenseMarkup = renderLicenseSection(data['License'].toString());
     licenseString = licenseString + licenseMarkup + "\n";
 
   }
   
-  //add body. ignore empty elements, title because it is already written and profile and email 
+  //add body. ignore empty elements, title and license because it is already written, profile and email will be handled separately
   let bodyString = "";
   for (var key in data) {
     if (data[key] != ""  && key != 'Title' && key != "Profile" && key != "Email" && key != "License") {
       bodyString = bodyString + `## ${key}\n${data[key]}\n`;
-      toc = toc + `[${key}](#${key.toLowerCase()})\n\n`
+      tocString = tocString + `[${key}](#${key.toLowerCase()})\n\n`
     }
   }
 
-  //add Question section if either profile or email are provided. Add them to the file if they are present
+  //add Question section if either profile or email are provided.
   if (data['Profile'] != "" || data['Email'] != "") {
     bodyString = bodyString + `## Questions\n`;
-    toc = toc + "[Questions](#questions)\n";
+    tocString = tocString + "[Questions](#questions)\n";
     data.Profile != "" ?  bodyString = bodyString + `[Github ${data['Profile']}](https://github.com/${data['Profile']})\n\n` : "";
     data.Email != "" ? bodyString = bodyString + `Email: ${data['Email']}\n` : "";
   }
-  return titleString + toc + licenseString + bodyString;
+  return titleString + tocString + licenseString + bodyString;
 }
 
 module.exports = generateMarkdown;
